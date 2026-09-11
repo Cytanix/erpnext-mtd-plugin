86b5df21731b59a36c431a2bcd36bf77eef372bc
Spirit Pearce <spirit.pearce@gmail.com>
2026-09-11 22:48:18 +0100

    feat: Add HMRC API settings

---
+++ erpnext_mtd/erpnext_mtd/doctype/hmrc_settings/hmrc_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Cytanix Ltd and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("HMRC Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ erpnext_mtd/erpnext_mtd/doctype/hmrc_settings/hmrc_settings.py
@@ -0,0 +1,22 @@
+# Copyright (c) 2026, Cytanix Ltd and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.model.document import Document
+
+
+class HMRCSettings(Document):
+	def validate(self):
+		if not self.enabled:
+			return
+
+		if not self.environment:
+			frappe.throw(
+				_("Environment is required when HMRC integration is enabled."))
+
+		if not self.client_id:
+			frappe.throw(
+				_("Client ID is required when HMRC integration is enabled."))
+
+		if not self.get_password("client_secret", raise_exception=False):
+			frappe.throw(_("Client Secret is required when HMRC integration is enabled."))
